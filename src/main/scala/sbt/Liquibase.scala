bd3cba6cbe27b70196d8288d6bf414431075ce35
Ricardo Franco <rmf.franco@gmail.com>
2013-07-14 21:43:30 -0300

    add liquibase parameters

---
+++ src/main/scala/sbt/Liquibase.scala
@@ -0,0 +1,26 @@
+package sbt
+
+import sbt.Def._
+import sbt.Keys._
+
+object Liquibase extends Plugin {
+
+  val liquibaseChangelog = settingKey[File]("The changelog file to use")
+  val liquibaseUsername = settingKey[String]("Database username")
+  val liquibasePassword = settingKey[String]("Database password")
+  val liquibaseUrl = settingKey[String]("Database URL")
+  val liquibaseDriver = settingKey[String]("Database driver class name")
+
+  //Optional Parameters
+  val liquibaseContexts = settingKey[Seq[String]]("ChangeSet contexts to execute")
+  val liquibaseDefaultSchemaName = settingKey[Option[String]]("Specifies the default schema to use for managed database objects and for Liquibase control tables")
+  val liquibaseLogLevel = settingKey[String]("Execution log level (debug, info, warning, severe, off)")
+
+  val liquibaseSettings: Seq[Setting[_]] = Seq(
+    liquibaseChangelog := (resourceDirectory in(ThisProject, Compile)).value / "migrations" / "changelog.xml",
+    liquibaseContexts := Nil,
+    liquibaseDefaultSchemaName := None,
+    liquibaseLogLevel := "warning"
+  )
+
+}
